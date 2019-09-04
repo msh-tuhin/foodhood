9a55601289aa9d185122ae697e6cf117009bf65d
Md. Sajjad Hossain <sajjadtuhin01@gmail.com>
2019-09-04 13:32:52 +0600

    Added new activity: AdvancedSearchDish.java

---
+++ app/src/main/java/com/example/tuhin/myapplication/AdvancedSearchDish.java
@@ -0,0 +1,14 @@
+package com.example.tuhin.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AdvancedSearchDish extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_advanced_search_dish);
+    }
+}
