54c3d124a60f08000522495277307713b5565fd1
Md. Sajjad Hossain <sajjadtuhin01@gmail.com>
2019-12-02 20:29:18 +0600

    added highlighting for name in general search

--- app/src/main/java/com/example/tuhin/myapplication/SearchFragment.java
+++ app/src/main/java/com/example/tuhin/myapplication/SearchFragment.java
@@ -2,6 +2,7 @@ package com.example.tuhin.myapplication;
 
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
 import android.graphics.Typeface;
 import android.net.Uri;
 import android.os.Bundle;
@@ -16,9 +17,11 @@ import de.hdodenhof.circleimageview.CircleImageView;
 import myapp.utils.AlgoliaCredentials;
 import myapp.utils.PictureBinder;
 
+import android.text.Spannable;
 import android.text.SpannableString;
 import android.text.SpannableStringBuilder;
 import android.text.Spanned;
+import android.text.style.ForegroundColorSpan;
 import android.text.style.StyleSpan;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -28,15 +31,22 @@ import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.algolia.instantsearch.core.events.QueryTextChangeEvent;
+import com.algolia.instantsearch.core.helpers.Highlighter;
 import com.algolia.instantsearch.core.helpers.Searcher;
+import com.algolia.instantsearch.ui.databinding.BindingHelper;
+import com.algolia.instantsearch.ui.databinding.RenderingHelper;
 import com.algolia.instantsearch.ui.helpers.InstantSearch;
 import com.algolia.instantsearch.ui.utils.ItemClickSupport;
 import com.algolia.instantsearch.ui.views.Hits;
 import com.algolia.instantsearch.ui.views.SearchBox;
 
+import org.json.JSONArray;
 import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.text.DecimalFormat;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class SearchFragment extends Fragment {
 
@@ -70,7 +80,7 @@ public class SearchFragment extends Fragment {
 
         instantSearch = new InstantSearch(getActivity(), searcher);
         instantSearch.search();
-        instantSearch.setSearchOnEmptyString(false);
+        // instantSearch.setSearchOnEmptyString(false);
 
         DividerItemDecoration dividerItemDecoration = new DividerItemDecoration(getActivity(),
                 DividerItemDecoration.VERTICAL);
@@ -122,7 +132,7 @@ public class SearchFragment extends Fragment {
                             break;
                     }
                 }catch (JSONException e){
-                    e.printStackTrace();
+                    Log.i("object_id", "no value found");
                 }
             }
         });
@@ -130,13 +140,16 @@ public class SearchFragment extends Fragment {
     }
 
     private void bindName(Hits hits, int position, View view){
+        Log.i("inside", "bindname");
         String name = "";
         try{
             name = hits.get(position).getString("name");
+            SpannableString highlighted = getHighlight(hits.get(position), "name", name);
+            ((TextView) view.findViewById(R.id.name)).setText(highlighted);
         }catch (JSONException e){
-            e.printStackTrace();
+            Log.i("name", "no value found");
+            ((TextView) view.findViewById(R.id.name)).setText("");
         }
-        ((TextView) view.findViewById(R.id.name)).setText(name);
     }
 
     private void bindRating(Hits hits, int position, View view){
@@ -144,7 +157,7 @@ public class SearchFragment extends Fragment {
         try{
             rating = hits.get(position).getDouble("rating");
         }catch (JSONException e){
-            e.printStackTrace();
+            Log.i("rating", "no value found");
             return;
         }
         TextView ratingTV = view.findViewById(R.id.rating);
@@ -163,7 +176,7 @@ public class SearchFragment extends Fragment {
         try{
             price = hits.get(position).getDouble("price");
         }catch (JSONException e){
-            e.printStackTrace();
+            Log.i("price", "no value found");
             return;
         }
         TextView priceTV = view.findViewById(R.id.price);
@@ -185,24 +198,27 @@ public class SearchFragment extends Fragment {
         // for persons
         try{
             currentTown = hits.get(position).getString("current_town");
-            SpannableString spannableAddress = new SpannableString(currentTown);
+            //SpannableString spannableAddress = new SpannableString(currentTown);
+            SpannableString spannableAddress = getHighlight(hits.get(position), "current_town",
+                    currentTown);
             spannableAddress.setSpan(new StyleSpan(Typeface.BOLD), 0,
                     currentTown.length(), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
             addressTV.setText(spannableAddress);
             addressLayout.setVisibility(View.VISIBLE);
             return;
         }catch (JSONException e){
-            e.printStackTrace();
+            Log.i("current_town", "no value found");
         }
 
         // for restaurants
         try{
             address = hits.get(position).getString("address");
-            addressTV.setText(address);
+            SpannableString spannedAddress = getHighlight(hits.get(position), "address", address);
+            addressTV.setText(spannedAddress);
             addressLayout.setVisibility(View.VISIBLE);
             return;
         }catch (JSONException e){
-            e.printStackTrace();
+            Log.i("address", "no value found");
         }
 
         // for dishes
@@ -210,7 +226,7 @@ public class SearchFragment extends Fragment {
             restaurantName = hits.get(position).getString("restaurant_name");
             restaurantAddress = hits.get(position).getString("restaurant_address");
         }catch (JSONException e){
-            e.printStackTrace();
+            Log.i("name_address", "no value found");;
         }
         if(restaurantName==null || restaurantName.equals("")) return;
         text = restaurantName;
@@ -218,8 +234,9 @@ public class SearchFragment extends Fragment {
             text += "\n" + restaurantAddress;
         }
         SpannableString spannedAddress = new SpannableString(text);
-        spannedAddress.setSpan(new StyleSpan(Typeface.BOLD), text.indexOf(restaurantName),
-                restaurantName.length(), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+        int start = text.indexOf(restaurantName);
+        int end = start + restaurantName.length();
+        spannedAddress.setSpan(new StyleSpan(Typeface.BOLD), start, end, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
         addressTV.setText(spannedAddress);
         addressLayout.setVisibility(View.VISIBLE);
     }
@@ -229,7 +246,7 @@ public class SearchFragment extends Fragment {
             String link = hits.get(position).getString("image_url");
             PictureBinder.bindPictureSearchResult((CircleImageView)view.findViewById(R.id.avatar), link);
         }catch (JSONException e){
-            e.printStackTrace();
+            Log.i("image_url", "no value found");;
         }
     }
 
@@ -260,7 +277,7 @@ public class SearchFragment extends Fragment {
                             break;
                     }
                 }catch (JSONException e){
-                    e.printStackTrace();
+                    Log.i("object_id", "no value found");
                 }
             }
         });
@@ -288,4 +305,34 @@ public class SearchFragment extends Fragment {
         //parentLayout.setOnClickListener(null);
     }
 
+    private SpannableString getHighlight(JSONObject hit, String attributeName, String attributeValue){
+        SpannableString highlightedText = new SpannableString(attributeValue);
+        try{
+            JSONObject highlightResult = hit.getJSONObject("_highlightResult");
+            JSONObject highlightedAttribute = highlightResult.getJSONObject(attributeName);
+            JSONArray matchedWords = highlightedAttribute.getJSONArray("matchedWords");
+            ArrayList<String> wordsList = new ArrayList<>();
+            if(matchedWords != null) {
+                for (int i = 0; i < matchedWords.length(); i++) {
+                    wordsList.add(matchedWords.getString(i));
+                }
+            }
+
+            String highlightable = null;
+            if(wordsList.size()>0){
+                highlightable = wordsList.get(0);
+                Log.i("highlightable", highlightable);
+            }
+            if(highlightable!=null){
+                int start = attributeValue.toLowerCase().indexOf(highlightable);
+                int end = start + highlightable.length();
+                highlightedText.setSpan(new ForegroundColorSpan(Color.BLUE),
+                        start, end, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+            }
+
+        }catch (JSONException e){
+            Log.e("_highlightResult", "no value found");
+        }
+        return highlightedText;
+    }
 }
