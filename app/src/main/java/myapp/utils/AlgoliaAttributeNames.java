c826fee0c9fecfb3a25c2b10a5299bd8f2f4d5c9
Md. Sajjad Hossain <sajjadtuhin01@gmail.com>
2020-01-20 04:13:23 +0600

    changed algoliaattributenames

--- app/src/main/java/myapp/utils/AlgoliaAttributeNames.java
+++ app/src/main/java/myapp/utils/AlgoliaAttributeNames.java
@@ -2,19 +2,19 @@ package myapp.utils;
 
 public class AlgoliaAttributeNames {
     public static final String ID = "objectID";
-    public static final String NAME = "name";
-    public static final String IMAGE_URL = "image_url";
+    public static final String NAME = "n";
+    public static final String IMAGE_URL = "i";
     public static final String TYPE = "type";
-    public static final String PRICE= "price";
-    public static final String RATING= "rating";
-    public static final String CITY= "city";
+    public static final String PRICE= "p";
+    public static final String RATING= "r";
+    public static final String CITY= "t";
     public static final String DISTRICT= "district";
-    public static final String CATEGORY= "category";
-    public static final String PERSON_CURRENT_TOWN= "current_town";
-    public static final String ADDRESS= "address";
-    public static final String DISH_PARENT_RESTAURANT_LINK= "restaurant";
-    public static final String DISH_PARENT_RESTAURANT_NAME= "restaurant_name";
-    public static final String DISH_PARENT_RESTAURANT_ADDRESS= "restaurant_address";
+    public static final String CATEGORY= "c";
+    public static final String PERSON_CURRENT_TOWN= "ct";
+    public static final String ADDRESS= "a";
+    public static final String DISH_PARENT_RESTAURANT_LINK= "re_l";
+    public static final String DISH_PARENT_RESTAURANT_NAME= "re_n";
+    public static final String DISH_PARENT_RESTAURANT_ADDRESS= "re_a";
     public static final String GEOLOC = "_geoloc";
     public static final String DISH_AVAILABILITY = "availability";
 }
